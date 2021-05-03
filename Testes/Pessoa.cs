f091734b6a60229ce11c00efa4d251d88a30acbc
zMaarrco <gamesmrco@gmail.com>
2021-05-03 10:31:58 -0300

    Testes

---
+++ Testes/Pessoa.cs
@@ -0,0 +1,15 @@
+namespace Testes
+{
+    public class Pessoa
+    {
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+
+        }
+        public string nome { get; set; }
+        public int idade { get; set; }
+
+    }
+}
---
+++ Testes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Testes
+{
+    class Program
+    {
+        static void Main(string[] args){
+            
+            /*LISTAS
+            List<Pessoa> pessoas = new List<Pessoa>();
+
+            pessoas.Add(new Pessoa("Arthur", 18));
+            pessoas.Add(new Pessoa("Bianca", 20));
+            pessoas.Add(new Pessoa("Celso", 15));
+            pessoas.Add(new Pessoa("Diego", 30));
+
+            foreach (Pessoa p in pessoas) {
+                Console.WriteLine(p.nome + " tem " + p.idade + " anos.");
+            }
+            */
+
+            //ARRAYS
+            int[] arrayInteiro = new int[10];
+            for (int i = 0; i < arrayInteiro.Length; i++){
+                arrayInteiro[i] = i + 1;
+            }
+            foreach (int x in arrayInteiro){
+                Console.WriteLine(x);
+            }
+            
+        }
+    }
+}
