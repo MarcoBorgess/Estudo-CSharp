e03f1954fc941affb79f52d7a6b53c511b218651
zMaarrco <gamesmrco@gmail.com>
2021-05-03 11:00:12 -0300

    ATV9

---
+++ ATV9/Math.cs
@@ -0,0 +1,18 @@
+namespace ATV9
+{
+    public class Math
+    {
+        public int soma (int x, int y){
+            return x + y;
+        }
+        public int sub (int x, int y){
+            return x - y;
+        }
+        public int mult (int x, int y){
+            return x * y;
+        }
+        public int div (int x, int y){
+            return x / y;
+        }
+    }
+}
---
+++ ATV9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ATV9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math matematica = new Math();
+            int[] resultados = new int[5];
+
+            resultados[0] = matematica.soma(1, 9); // 10
+            resultados[1] = matematica.sub(20, 9); // 11
+            resultados[2] = matematica.div(24, 2); // 12
+            resultados[3] = matematica.mult(13, 1);// 13
+            resultados[4] = matematica.mult(7, 2); // 14
+
+            foreach (int x in resultados){
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
